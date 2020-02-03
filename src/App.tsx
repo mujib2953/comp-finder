0683ec8c5fc832d4b3c725c4da84684236a4f5bc
Mujibur Rehman <mujibur.rehman@citiustech.com>
2020-02-03 20:45:37 +0530

    Basic React App with working Redux completed.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
-import './App.css';
+import { Provider } from "react-redux";
+
+import { store } from "./store/store";
+
+import Add from "./components/Add";
+import View from "./components/View";
 
 const App = () => {
 	return (
-		<div className="App">
-			Hello world.
-		</div>
+		<Provider
+			store={ store }
+		>
+			<Add />
+			<View />
+		</Provider>
 	);
 }
 
